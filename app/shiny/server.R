21ee6e97de08516d47f05d48f5273918e0093840
GD <gd.dev@libertymail.net>
2020-12-04 10:07:48 +0100

    enable verbosity

--- app/shiny/server.R
+++ app/shiny/server.R
@@ -1,6 +1,7 @@
 library(shiny)
 server <- function(input, output, session) {
     require(diyabcGUI)
+    diyabcGUI::enable_logging()
     diyabcGUI::redirect_output()
     diyabcGUI::diyabc_server(input, output, session)
 }
