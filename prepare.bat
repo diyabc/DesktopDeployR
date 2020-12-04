7d70713331e026b589da5c0a2354e8e3e7cefd5f
GD <gd.dev@libertymail.net>
2020-12-04 15:59:42 +0100

    disable cmd.exe echoing

--- prepare.bat
+++ prepare.bat
@@ -1,2 +1,3 @@
+@echo off
 dist\\R-Portable\\App\\R-Portable\\bin\\Rscript.exe generate_Renviron.R
 dist\\R-Portable\\App\\R-Portable\\bin\\Rscript.exe prepare.R
--- run_local_app_debug.bat
+++ run_local_app_debug.bat
@@ -1,2 +1,3 @@
+@echo off
 dist\\R-Portable\\App\\R-Portable\\bin\\Rscript.exe generate_Renviron.R
 dist\\R-Portable\\App\\R-Portable\\bin\\Rscript.exe run_local_app_debug.R
